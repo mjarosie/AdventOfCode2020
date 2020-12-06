4d04a4766ecc4d4569936a2ae70f03385b1ce305
Maciej Jarosiewicz <mjarosie@users.noreply.github.com>
2020-12-06 22:43:18 +0000

    day 4

---
+++ day_04/part_1/src/main/scala/example/Hello.scala
@@ -0,0 +1,101 @@
+package example
+
+import scala.io.Source
+
+sealed trait Document
+
+case class Passport(byr: String, iyr: String, eyr: String, hgt: String, hcl: String, ecl: String, pid: String, cid: String) extends Document
+
+object Passport {
+  def mapContainsAllProperties(props: Map[String, String]) : Boolean = {
+    if (props.contains("byr") && props.contains("iyr") && props.contains("eyr") && 
+        props.contains("hgt") && props.contains("hcl") && props.contains("ecl") && 
+        props.contains("pid") && props.contains("cid")) {
+        true
+    }
+    else {
+      false
+    }
+  }
+}
+
+case class NorthPoleCredentials (byr: String, iyr: String, eyr: String, hgt: String, hcl: String, ecl: String, pid: String) extends Document
+
+object NorthPoleCredentials {
+  def mapContainsAllProperties(props: Map[String, String]) : Boolean = {
+    if (props.contains("byr") && props.contains("iyr") && props.contains("eyr") && 
+        props.contains("hgt") && props.contains("hcl") && props.contains("ecl") && 
+        props.contains("pid")) {
+        true
+    }
+    else {
+      false
+    }
+  }
+}
+
+case class Invalid() extends Document
+
+object Invalid {
+  def mapContainsAllProperties(props: Map[String, String]) : Boolean = {
+    true
+  }
+}
+
+object Hello extends App {
+  val filename = "input.txt"
+  val inputLines = Source.fromFile(filename).getLines().toList
+
+  val (lastItemProperties, items) = inputLines.foldLeft(Map[String, String](), List[Map[String, String]]()) ((acc: (Map[String, String], List[Map[String, String]]), line: String) => {
+    val currentItemPropertiesAccumulator = acc._1
+    val itemsSoFar = acc._2
+    val newAccState = line match {
+      case "" => {
+        (Map[String, String](), currentItemPropertiesAccumulator :: itemsSoFar)
+      }
+      case documentPropertiesLine => {
+        val documentProperties = documentPropertiesLine.split(" ")
+        val currentLineKeyValuePairs = documentProperties.map(x => {
+          val keyValuePair = x.split(":")
+          (keyValuePair(0), keyValuePair(1))
+        })
+        
+        val accumulatorMapUpdate = currentLineKeyValuePairs.foldLeft(Map[String, String]()) ((props, keyValuePair) => {
+          props.updated(keyValuePair._1, keyValuePair._2)
+        })
+        
+        val updatedCurrentItemAcc = currentItemPropertiesAccumulator ++ accumulatorMapUpdate
+        (updatedCurrentItemAcc, itemsSoFar)
+      }
+    }
+    newAccState
+  })
+
+  val allItems = lastItemProperties :: items
+
+  val documents = allItems.map(itemProperties => {
+    if (Passport.mapContainsAllProperties(itemProperties)) {
+      Passport( itemProperties("byr"), itemProperties("iyr"), itemProperties("eyr"),
+                itemProperties("hgt"), itemProperties("hcl"), itemProperties("ecl"), 
+                itemProperties("pid"), itemProperties("cid") )
+    }
+    else if (NorthPoleCredentials.mapContainsAllProperties(itemProperties)) {
+      NorthPoleCredentials( itemProperties("byr"), itemProperties("iyr"), itemProperties("eyr"),
+                itemProperties("hgt"), itemProperties("hcl"), itemProperties("ecl"), 
+                itemProperties("pid") )
+    } else Invalid()
+  })
+
+  val validDocumentsCount = documents.foldLeft(0)((validItemsCount, currentItem) => {
+      val currentItemValid = currentItem match {
+        case _: Passport => true
+        case _: NorthPoleCredentials => true
+        case _: Invalid => false
+      }
+      if (currentItemValid) (validItemsCount + 1) else validItemsCount
+    }
+  )
+  
+  println(validDocumentsCount)
+}
+
